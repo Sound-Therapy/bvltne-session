93294c3b009536654606fcadcc14a56dfe6df65d
MasonMcKayla <iameyl@gmail.com>
2026-07-21 13:57:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1042,12 +1042,23 @@ async function downloadWav(fileName) {
         return;
     }
 
+    const response = await fetch(data.signedUrl);
+
+    const blob = await response.blob();
+
+    const url = URL.createObjectURL(blob);
+
     const a = document.createElement("a");
-    a.href = data.signedUrl;
+
+    a.href = url;
     a.download = fileName;
 
     document.body.appendChild(a);
+
     a.click();
+
     a.remove();
+
+    URL.revokeObjectURL(url);
 
 }
